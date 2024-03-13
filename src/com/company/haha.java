42f55838c45e3970d756a67d44aaf9493dbaa289
Thanh Hieu Pham <117836131+hieupham04@users.noreply.github.com>
2024-03-13 22:32:37 +0700

    Update haha.java

--- src/com/company/haha.java
+++ src/com/company/haha.java
@@ -4,5 +4,6 @@ public class haha {
     public static void main(String[] args) {
         System.out.println("Sơn tùng mtp");
          System.out.println("Sơn tùng mtp2");
+         System.out.println("tadaa");
     }
 }
